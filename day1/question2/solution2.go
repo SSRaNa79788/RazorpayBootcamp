45099d8fa51b90e3a75f1c329dfc95988a3df236
subhram <subhramsubhrajyoti111@gmail.com>
2021-07-20 19:18:40 +0530

    done with q2 partially

--- day1/question2/solution2.go
+++ day1/question2/solution2.go
@@ -25,6 +25,27 @@ func (cur *node)postOrder(){
 	fmt.Print(cur.char)
 	cur.preOrder(cur.left)
 }
+func solve(s string,l int,r int ) *node {
+	if l==r {
+		cur := node(s[l],nil,nil)
+		return &cur
+	} else if l==r-2 {
+		cur:=node(s[l+1])
+		cur.left=solve(s,l,l)
+		cur.right=solve(s,r,r)
+		return &cur
+	} else {
+		//case 1: s[i+1] wil be the current treenode
+		if pref()<=pref() {
+			cur:=node(s[l+1])
+			cur.left=solve(s,l,l)
+			cur.right=solve(s,l+2,r)
+		} else { //case 2: s[i+1] will be in the left part of the tree
+			cur:=node(s[l+3])
+			cur.left=solve(s,l,)
+		}
+	}
+}
 func main(){
 	var exprsn="a+b"
 
