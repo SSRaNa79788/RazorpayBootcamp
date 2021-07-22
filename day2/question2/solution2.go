71c37361c1f9ca9a7d9be2d3488c6be7d0613fdb
subhram <subhramsubhrajyoti111@gmail.com>
2021-07-22 16:58:19 +0530

    done with question 2

--- day2/question2/solution2.go
+++ day2/question2/solution2.go
@@ -1,1 +1,24 @@
-package question2
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+var wg sync.WaitGroup
+func add(sum *int, rating int){
+	*sum+=rating
+	wg.Done()
+}
+
+func main(){
+	ratings := []int{2,5,3,4,2,3,2,3,4,5,5,5,3,1,4}
+	sum:=0
+	l:=len(ratings)
+	for _,rating:=range ratings{
+		wg.Add(1)
+		go add(&sum,rating)
+	}
+
+	wg.Wait()
+	fmt.Println(float64(sum)/float64(l))
+}
