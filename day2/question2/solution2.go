73f24c1b08e63602db70a91fd260b68f394a3fdf
subhram <subhramsubhrajyoti111@gmail.com>
2021-07-23 08:29:44 +0530

    handled cuncurrency ; added the ratings at random time

--- day2/question2/solution2.go
+++ day2/question2/solution2.go
@@ -4,20 +4,36 @@ import (
 	"fmt"
 	"math/rand"
 	"sync"
+	"time"
 )
+
 var wg sync.WaitGroup
+var mu sync.Mutex
+
 func add(sum *int, rating int){
+
+	someRandomTime := int(rand.Intn(1000))
+	for i:=0;i<someRandomTime ;i++{
+		time.Sleep(time.Microsecond)
+	}
+	//time.Sleep(someRandomTime*time.Millisecond)// I am waiting for some random time duration
+
+	mu.Lock()
 	*sum+=rating
+	mu.Unlock()
+
 	wg.Done()
 }
 
 func main(){
+	rand.Seed(time.Now().UnixNano()) //to get different pseudo random numbers
+
 	var ratings []int
 
 	//generating random ratings
 	for i:=0;i<200;i++{
 		someRandomNumber := rand.Intn(5)
-		ratings[i]=someRandomNumber
+		ratings=append(ratings,someRandomNumber)
 	}
 
 	sum:=0
